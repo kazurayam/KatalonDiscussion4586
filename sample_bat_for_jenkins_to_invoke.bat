44f84aa52242224c28e322e7e807a385d4b22eda
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-10-18 09:46:33 +0900

    modified KATALONSTUDIO_HOME value

--- sample_bat_for_jenkins_to_invoke.bat
+++ sample_bat_for_jenkins_to_invoke.bat
@@ -2,6 +2,6 @@ echo GIT_BRANCH="%GIB_BRANCH%"   # e.g. 'origin/master'
 set url=https://%GIT_BRANCH:origin/=%-devui.windsorcircle.com
 echo url=%url%
 
-set KATALONSTUDIO_HOME="/Applications/Katalon Studio.app/Contents/MacOS"
+set KATALONSTUDIO_HOME="C:\Katalon_Studio_Windows_64-5.7.0"
 .\run_console_mode.bat
 
