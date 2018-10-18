44f95d8ebcf95c5d2ba94a6a9520de99bf7ed615
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-10-18 09:41:38 +0900

    add .bat files

---
+++ sample_bat_for_jenkins_to_invoke.bat
@@ -0,0 +1,7 @@
+echo GIT_BRANCH="%GIB_BRANCH%"   # e.g. 'origin/master'
+set url=https://%GIT_BRANCH:origin/=%-devui.windsorcircle.com
+echo url=%url%
+
+set KATALONSTUDIO_HOME="/Applications/Katalon Studio.app/Contents/MacOS"
+./run_console_mode.bat
+
