d2e8297ab349345de8a3ecd048070f6fd3a3a361
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-10-18 09:58:11 +0900

    update

--- run_console_mode.bat
+++ run_console_mode.bat
@@ -9,7 +9,7 @@ echo PROJECT_DIR=%PROJECT_DIR%
 
 cd "%KATALONSTUDIO_HOME%"
 
-.\katalon.exe -noSplash -runMode=console -summaryReport -projectPath="%PROJECT_DIR%" -testSuiteCollectionPath="Test Suites\TCS" --config -proxyOption=MANUAL_CONFIG -proxy.server.type=HTTP -proxy.server.address="172.24.2.10" -proxy.server.port="8080"
+.\katalon.exe -noSplash -runMode=console -summaryReport -projectPath="%PROJECT_DIR%" -testSuiteCollectionPath="Test Suites\TSC" --config -proxyOption=MANUAL_CONFIG -proxy.server.type=HTTP -proxy.server.address="172.24.2.10" -proxy.server.port="8080"
 set exitCode=%ERRORLEVEL%
 
 cd /d "%PROJECT_DIR%"
