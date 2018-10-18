2205eff38ee97ae412fafe74f50196213af9f22e
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-10-18 09:43:25 +0900

    typo

--- sample_bat_for_jenkins_to_invoke.bat
+++ sample_bat_for_jenkins_to_invoke.bat
@@ -3,5 +3,5 @@ set url=https://%GIT_BRANCH:origin/=%-devui.windsorcircle.com
 echo url=%url%
 
 set KATALONSTUDIO_HOME="/Applications/Katalon Studio.app/Contents/MacOS"
-./run_console_mode.bat
+.\run_console_mode.bat
 
